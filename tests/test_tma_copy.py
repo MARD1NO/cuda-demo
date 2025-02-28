57ebae1c4b39e1128bb4834f4a3cf68b0043cc21
MARD1NO <359521840@qq.com>
2025-02-28 14:56:29 +0800

    fix not divisible k

--- tests/test_tma_copy.py
+++ tests/test_tma_copy.py
@@ -4,7 +4,11 @@ import numpy as np
 from loguru import logger 
 
 np.random.seed(0)
-np_x = np.random.uniform(-1, 1, (1024, 256))
+# np_x = np.random.uniform(-1, 1, (1024, 256))
+# np_x = np.random.uniform(-1, 1, (1024, 256 + 64))
+np_x = np.random.uniform(-1, 1, (1024, 256 + 32))
+
+
 
 x = torch.tensor(np_x, dtype=torch.float32, device='cuda')
 y = torch.zeros(np_x.shape, dtype=torch.float32, device='cuda')
